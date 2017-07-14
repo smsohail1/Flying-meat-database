258164c4f5f87a56e9894743b9ac82dbfa7c3c4e
Abdul Ahad <abdulahad@Abduls-iMac.local>
2017-07-14 04:48:53 -0700

    Version 1.0

---
+++ database(fmdb)-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "FMDB.h"
+#import "FMDatabase.h"
+#import "FMDatabaseAdditions.h"
+#import "FMDatabasePool.h"
+#import "FMDatabaseQueue.h"
+#import "FMResultSet.h"
--- database(fmdb)/ViewController.swift
+++ database(fmdb)/ViewController.swift
@@ -9,17 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    var databasePath = String()
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var address: UITextField!
+    
+    @IBOutlet weak var phoneNo: UITextField!
+    
+    @IBOutlet weak var status: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let filemgr = FileManager.default
+        let dirPaths = filemgr.urls(for: .documentDirectory,
+                                    in: .userDomainMask)
+        
+        databasePath = dirPaths[0].appendingPathComponent("contacts.db").path
+        
+        if !filemgr.fileExists(atPath: databasePath as String) {
+            
+            let contactDB = FMDatabase(path: databasePath as String)
+            
+            if contactDB == nil {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+            
+            if (contactDB.open()) {
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)"
+                if !(contactDB.executeStatements(sql_stmt)) {
+                    print("Error: \(contactDB.lastErrorMessage())")
+                }
+                contactDB.close()
+            } else {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func saveBtn(_ sender: Any) {
+       
+        let contactDB = FMDatabase(path: databasePath as String)
+        
+        if (contactDB.open()) {
+            
+            let insertSQL = "INSERT INTO CONTACTS (name, address, phone) VALUES ('\(name.text!)', '\(address.text!)', '\(phoneNo.text!)')"
+            
+            let result = contactDB.executeUpdate(insertSQL,
+                                                  withArgumentsIn: [])
+            
+            if !result {
+                status.text = "Failed to add contact"
+                print("Error: \(contactDB.lastErrorMessage())")
+            } else {
+                status.text = "Contact Added"
+                name.text = ""
+                address.text = ""
+                phoneNo.text = ""
+            }
+        } else {
+            print("Error: \(contactDB.lastErrorMessage())")
+        }
+    }
+    
+    @IBAction func findBtn(_ sender: Any) {
+        
+        let contactDB = FMDatabase(path: databasePath as String)
+        
+        if (contactDB.open()) {
+            let querySQL = "SELECT address, phone FROM CONTACTS WHERE name = '\(name.text!)'"
+            
+            let results:FMResultSet? = contactDB.executeQuery(querySQL,
+                                                               withArgumentsIn: [])
+            
+            if results?.next() == true {
+                address.text = results?.string(forColumn: "address")
+                phoneNo.text = results?.string(forColumn: "phone")
+                status.text = "Record Found"
+            } else {
+                status.text = "Record not found"
+                address.text = ""
+                phoneNo.text = ""
+            }
+            contactDB.close()
+        } else {
+            print("Error: \(contactDB.lastErrorMessage())")
+        }
+        
+    }
+    
 
 }
 
